2a23acdabc261bb45b3feabc66cf4c5a9559c922
sagax <sagax@inbox.ru>
2016-08-08 01:58:01 +0300

    review: data/lib/chrome-storage-emulate.js

--- data/lib/chrome-storage-emulate.js
+++ data/lib/chrome-storage-emulate.js
@@ -1,80 +1,74 @@
 if (typeof chrome == 'undefined') {
+  var MC = function() {
+    var that = this;
+    addon.port.on("message", function(message) {
+      that.msg_handler(message);
+    });
+  };
 
-	var MC = function() {
-	    var that = this;
+  MC.prototype = {
+    messages_callbacks: [],
+    func_sendMessage: function(msg, field, value, callback) {
+      this.messages_callbacks.push({
+        "func": msg,
+        "field": field,
+        "callback": callback
+      });
 
-        addon.port.on("message", function(message) {
-            that.msg_handler(message);
-        });
-	};
+      port_sendMessage({"doing": "function", "func": msg, "field": field, "value": value});
+    },
 
-	MC.prototype = {
-		messages_callbacks : [],
+    msg_handler: function(msg) {
+      var that = this;
 
-        func_sendMessage : function(msg, field, value, callback) {
-        	this.messages_callbacks.push({
-        	    "func"		: msg,
-        		"field" 	: field,
-        		"callback" 	: callback
-        	});
+      if (msg.doing == 'answer_function') {
+        var c = this.messages_callbacks;
+        var j = get_el_by_fields(c, {
+          "func": msg.func,
+          "field": msg.field
+        }
+        );
 
-    		port_sendMessage({"doing": "function", "func" : msg, "field" : field, "value" : value});
+        if (j != -1) {
+          var callback = c.splice(j, 1);
+          callback[0]['callback'].call(that, msg.value);
+        }
+      }
+    }
+  };
+
+  var mc = new MC();
+  window.chrome = {
+    storage: {
+      sync: {
+        set: function(obj, callback) {
+          mc.func_sendMessage('storage.set', 'obj', obj, function() {
+            callback();
+          });
         },
 
-        msg_handler : function(msg) {
-	    	var that = this;
+        get: function(key, callback) {
+          mc.func_sendMessage('storage.get', 'key', key, function(val) {
+            callback(val);
+          });
+        },
 
-            if (msg.doing == 'answer_function') {
-                var c = this.messages_callbacks;
-                var j = get_el_by_fields(c, {
-            		"func" 		: msg.func,
-            		"field" 	: msg.field
-            		}
-            	);
+        remove: function(key, callback) {
+          mc.func_sendMessage('storage.remove', 'key', key, function() {
+            callback();
+          });
+        },
 
-            	if (j != -1) {
-            		var callback = c.splice(j, 1);
-            		callback[0]['callback'].call(that, msg.value);
-            	}
-            }
+        clear: function(callback) {
+          mc.func_sendMessage('storage.clear', 'all', '', function() {
+            callback();
+          });
         }
+      }
+    },
 
-	};
-
-    var mc = new MC();
-
-
-	window.chrome = {
-		storage : {
-			sync : {
-				set : function(obj, callback) {
-					mc.func_sendMessage('storage.set', 'obj', obj, function() {
-						callback();
-					});
-				},
-
-				get : function(key, callback) {
-					mc.func_sendMessage('storage.get', 'key', key, function(val) {
-						callback(val);
-					});
-				},
-
-				remove : function(key, callback) {
-					mc.func_sendMessage('storage.remove', 'key', key, function() {
-						callback();
-					});
-				},
-
-				clear : function(callback) {
-					mc.func_sendMessage('storage.clear', 'all', '', function() {
-						callback();
-					});
-				}
-			}
-		},
-
-		runtime : {
-			lastError : false
-		}
-	};
+    runtime: {
+      lastError: false
+    }
+  };
 }
